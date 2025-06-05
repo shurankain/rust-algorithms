5d8222a1db94dad55744f916efd27d014a30424f
Oleksandr_Husiev <ohusiev@icloud.com>
2025-06-05 05:37:36 +0200

    feat: linear search

---
+++ searching/src/linear_search.rs
@@ -0,0 +1,29 @@
+pub fn linear_search(data: &[i32], search_val: i32) -> Option<usize> {
+    for (i, curr_val) in data.iter().enumerate() {
+        if *curr_val == search_val {
+            return Option::Some(i);
+        }
+    }
+    Option::None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_present() {
+        let input = vec![12, 34, 17, 9, 24, 3];
+        let result = linear_search(&input, 17).expect("Incorrect result state");
+
+        assert_eq!(2, result);
+    }
+
+    #[test]
+    fn test_not_present() {
+        let input = vec![12, 34, 17, 9, 24, 3];
+        let result = linear_search(&input, 11);
+
+        assert_eq!(None, result);
+    }
+}
