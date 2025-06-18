ccf9b2ae266bd35714c9a308cc69727f0ce9e8b8
Oleksandr_Husiev <ohusiev@icloud.com>
2025-06-18 05:34:44 +0200

    feat: impl default instead of new for fibonacci

--- dynamic_programming/src/fibonacci_with_memoization.rs
+++ dynamic_programming/src/fibonacci_with_memoization.rs
@@ -6,13 +6,6 @@ pub struct Fibonacci {
 }
 
 impl Fibonacci {
-    pub fn new() -> Self {
-        let mut memo = HashMap::new();
-        memo.insert(0, 0);
-        memo.insert(1, 1);
-        Self { memo }
-    }
-
     pub fn fib(&mut self, n: u64) -> u64 {
         if let Some(&value) = self.memo.get(&n) {
             return value;
@@ -23,13 +16,22 @@ impl Fibonacci {
     }
 }
 
+impl Default for Fibonacci {
+    fn default() -> Self {
+        let mut memo = HashMap::new();
+        memo.insert(0, 0);
+        memo.insert(1, 1);
+        Self { memo }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_fib_small() {
-        let mut fib = Fibonacci::new();
+        let mut fib = Fibonacci::default();
         assert_eq!(fib.fib(0), 0);
         assert_eq!(fib.fib(1), 1);
         assert_eq!(fib.fib(2), 1);
@@ -39,7 +41,7 @@ mod tests {
 
     #[test]
     fn test_fib_larger() {
-        let mut fib = Fibonacci::new();
+        let mut fib = Fibonacci::default();
         assert_eq!(fib.fib(50), 12586269025);
     }
 }
